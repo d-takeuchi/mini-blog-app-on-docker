f7531e811799b46cf885dcb4ade753b188c2bb85
d-takeuchi <kuronk.kuro3@gmail.com>
2023-09-22 00:38:31 +0900

    mod: dbセットアップ情報の修正

--- docker/db/init/1.create-db.sql
+++ docker/db/init/1.create-db.sql
@@ -1,1 +1,27 @@
-CREATE DATABASE udemy_db DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_bin;
+CREATE DATABASE `oneline_bbs` DEFAULT CHARACTER SET utf8;
+
+USE oneline_bbs;
+CREATE TABLE user(
+    id INTEGER AUTO_INCREMENT,
+    user_name VARCHAR(20) NOT NULL,
+    password VARCHAR(40) NOT NULL,
+    created_at DATETIME,
+    PRIMARY KEY(id),
+    UNIQUE KEY user_name_index(user_name)
+) ENGINE = INNODB;
+CREATE TABLE following(
+    user_id INTEGER,
+    following_id INTEGER,
+    PRIMARY KEY(user_id, following_id)
+) ENGINE = INNODB;
+CREATE TABLE status(
+    id INTEGER AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    body VARCHAR(255),
+    created_at DATETIME,
+    PRIMARY KEY(id),
+    INDEX user_id_index(user_id)
+) ENGINE = INNODB;
+ALTER TABLE following ADD FOREIGN KEY (user_id) REFERENCES user(id);
+ALTER TABLE following ADD FOREIGN KEY (following_id) REFERENCES user(id);
+ALTER TABLE status ADD FOREIGN KEY (user_id) REFERENCES user(id);
--- docker/db/init/2.grant-user.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'udemy_user'@'%' IDENTIFIED BY 'udemy_pass';
-GRANT ALL PRIVILEGES ON udemy_db.* TO 'udemy_user'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
